30ec5415fbee5f3facb531442aebc328ee76cbe1
thetyper101 <ewcgong@hotmail.com>
2022-11-12 21:59:38 -0500

    Added some test images and tweaked aiming of player weapon.
    Fixed rotation of player weapon and added some test images as placeholder sprites. I plan to enable shooting and possibly tweak player movement controls.

--- Unity Files/DemonReach/Assets/Scripts/GameObject Scripts/WeaponControls.cs
+++ Unity Files/DemonReach/Assets/Scripts/GameObject Scripts/WeaponControls.cs
@@ -7,18 +7,31 @@ public class WeaponControls : MonoBehaviour
     // Start is called before the first frame update
 
     public Camera cam;
-    private Vector3 cursorPos;
+    private Vector3 mousePos;
     public GameObject projectile;
+    public Transform projectileTransform;
     public bool canFire;
     public float timeBetweenFiring;
     private float timer;
 
     private void Update()
     {
-        cursorPos = cam.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 rotation = cursorPos - transform.position;
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        
+        Vector3 rotation = mousePos - transform.position;
         float degree = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, degree);
+        //transform.Rotate(new Vector3(0.0f, 0.0f, degree));
+        //transform.eulerAngles = new Vector3(0, 0, degree);
+        /*
+        if(mousePos.x < transform.position.x)
+        {
+            transform.eulerAngles = new Vector3(transform.rotation.x, 180f, transform.rotation.z);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(transform.rotation.x, 0f, transform.rotation.z);
+        }*/
     }
 
     void Fire()
@@ -28,8 +41,14 @@ public class WeaponControls : MonoBehaviour
             timer += Time.deltaTime;
             if(timer > timeBetweenFiring)
             {
-
+                canFire = true;
+                timer = 0;
             }
+        }
+        if(Input.GetMouseButton(0) && canFire == true)
+        {
+            canFire = false;
+            Instantiate(projectile, projectileTransform.position, Quaternion.identity);
         }
     }
 }
