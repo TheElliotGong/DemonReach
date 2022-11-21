bcaa8dd46d304fb6e2592890a1b9b7f9ffaac36e
thetyper101 <ewcgong@hotmail.com>
2022-11-20 21:31:54 -0500

    Created a Player class singleton that can easily be accessed by all scripts. Filled out Upgrade Script and Upgrade Manager.
    Added a public static instance of Player class. Filled out Upgrade Manager and Upgrade with code regarding the random generation of available upgrades.

--- Unity Files/DemonReach/Assets/Scripts/Managers/HealthBar.cs
+++ Unity Files/DemonReach/Assets/Scripts/Managers/HealthBar.cs
@@ -8,20 +8,17 @@ public class HealthBar : MonoBehaviour
     // Start is called before the first frame update
 
     private Image healthBar;
-    public float currentHealth;
-    public float maxHealth;
-    public Player playerScript;
+    
     
     void Start()
     {
-        healthBar = GetComponent<Image>();
+        healthBar = gameObject.GetComponent<Image>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentHealth = playerScript.hp;
-        healthBar.fillAmount = currentHealth / maxHealth;
+        healthBar.fillAmount = Player.instance.hp / Player.instance.maxHP;
     }
 }
