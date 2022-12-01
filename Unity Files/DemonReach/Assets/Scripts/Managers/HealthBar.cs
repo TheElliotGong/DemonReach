3ce2fea096df8e1377d38171fa523e738112ae44
thetyper101 <ewcgong@hotmail.com>
2022-12-01 15:05:11 -0500

    Fixed hp bar script to make it applicable to multiple game objects

--- Unity Files/DemonReach/Assets/Scripts/Managers/HealthBar.cs
+++ Unity Files/DemonReach/Assets/Scripts/Managers/HealthBar.cs
@@ -6,19 +6,37 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     // Start is called before the first frame update
-
-    private Image healthBar;
-    private Player player;
     
+    private Image healthBar;
+ 
+    private Player player;
+    private Lycant lycant;
+    public bool isPlayer;
     void Start()
     {
         healthBar = gameObject.GetComponent<Image>();
-        player = GameObject.Find("Player").GetComponent<Player>();
+        if(isPlayer == true)
+        { 
+            player = GameObject.Find("Player").GetComponent<Player>();
+        }
+        else 
+        {
+            lycant = GameObject.Find("Lycan").GetComponent<Lycant>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        healthBar.fillAmount = player.hp / player.maxHP;
+        if(isPlayer)
+        {
+            healthBar.fillAmount = player.hp / player.maxHP;
+
+        }
+        else
+        {
+            healthBar.fillAmount = lycant.health / lycant.maxHealth;
+        }
+        
     }
 }
